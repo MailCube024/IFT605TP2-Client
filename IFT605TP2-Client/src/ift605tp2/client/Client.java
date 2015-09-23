c999e5ea6076348af3fa403049d7d15edbbbf450
Road0Kamelot <Benoit.Jeunehomme@usherbrooke.ca>
2015-09-23 18:25:52 -0400

    Working test client

--- IFT605TP2-Client/src/ift605tp2/client/Client.java
+++ IFT605TP2-Client/src/ift605tp2/client/Client.java
@@ -5,6 +5,14 @@
  */
 package ift605tp2.client;
 
+import constants.Constants;
+import contracts.IDerivationCommand;
+import java.rmi.Naming;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import udes.ds.agent.Constant;
+import udes.ds.agent.Equation;
+
 /**
  *
  * @author Michaël
@@ -15,7 +23,16 @@ public class Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String host = "localhost";
+        try {
+            Registry registry = LocateRegistry.getRegistry(host, Constants.RMI_PORT);
+            IDerivationCommand stub = (IDerivationCommand) registry.lookup(Constants.DERIVATION_ENGINE_ID);
+            //IDerivationCommand stub = (IDerivationCommand) Naming.lookup("rmi://localhost:" + Constants.RMI_PORT + "/" + Constants.DERIVATION_ENGINE_ID);
+            Equation response = stub.Derivate(new Constant(7));
+            response.printUserReadable();
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }    
     }
-    
 }
